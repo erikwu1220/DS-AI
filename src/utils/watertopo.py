de8208407c3e6ebd0adbc586168ec1a6d37ae8bf
Patrick de Bruijn <pjdebruijn@live.nl>
2024-01-24 17:42:07 +0100

    Added WaterTopo class
    Removed unused method from simulation class.

---
+++ src/utils/watertopo.py
@@ -0,0 +1,70 @@
+import os
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+class WaterTopo():
+    @staticmethod
+    def load_simulations(save_folder, sim_amount, number_grids, random_state=42):
+        """
+        Static method to create a simulation from a filepath.
+        - save_folder: string, that provides the filepath of where to look for the following folders:
+            - DEM   (topography)
+            - VX    (x-velocities)
+            - VY    (y-velocities)
+            - WD (waterlevels)
+        - sim_amount: int, amount of simulations to load.
+        - number_grid: int, predefined grid dimension to help shape the data in the correct form.
+
+        return:
+        - List of Simulation objects.
+        """
+        fnames = os.listdir(save_folder + "\\DEM")
+
+        if fnames[0].isupper():
+            name_dict = {"DEM":"DEM",
+                         "WD": "WD"}
+        else:
+            name_dict = {"DEM":"dem",
+                         "WD": "wd"}
+
+        if sim_amount > len(fnames):
+            raise Exception(f"Please select an amount smaller than {len(fnames)}.")
+        
+        np.random.seed(random_state)
+        idx = random.sample(range(len(fnames)), sim_amount)
+        sims = []
+
+        for i in range(sim_amount):
+            name = fnames[idx[i]]
+
+            coords = np.loadtxt(f"{save_folder}\\DEM\\" + name_dict["DEM"] + name[3:])[:, :2]
+            topo = np.loadtxt(f"{save_folder}\\DEM\\" + name_dict["DEM"] + name[3:])[:, 2].reshape(number_grids,number_grids)
+            wd = np.loadtxt(f"{save_folder}\\WD\\" + name_dict["WD"] + name[3:]).reshape(-1,number_grids,number_grids)
+
+            sims.append(WaterTopo(coords, topo, wd))
+
+        return sims
+    
+    def __init__(self, coordinates, topography, wd):
+        """
+        A class that contains for a simulation:
+        - topography:   2D array
+        - wd:           3D array
+        - vx:           3D array
+        - vy:           3D array
+        """
+        self.coordinates = coordinates
+        self.topography = topography
+        self.wd = wd
+
+    def return_timestep(self, timestep):
+        """
+        timestep: timestep at which you want the data to be returned
+
+        returns: wd, vx, vy at specified timestep
+        """
+        return self.wd[timestep]
