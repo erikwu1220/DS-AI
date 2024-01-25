f002efde717e7400dbd2018a57a40335ba43d2ba
Patrick de Bruijn <pjdebruijn@live.nl>
2024-01-25 12:25:48 +0100

    Added sequence creation method
    - seq method
    - started debugging ConvLSTM
    - changed some parts of simulation and watertopo classes

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,2 +1,56 @@
+import numpy as np
+from utils.watertopo import WaterTopo
+from utils.simulation import Simulation
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def create_sequence(series, T=5, H=1):
+    """
+    Create sequence of input X and output Y from a last of Simulation or WaterTopo objects:
+    - series: list of Simulation or WaterTopo objects
+
+
+    - X: input for NN, shape [T * channels * height * width]
+    - Y: output for NN [H * height * width]
+    """
+    if isinstance(series[0], Simulation):
+        channels = 4
+    elif isinstance(series[0], WaterTopo):
+        channels = 2
+    else:
+        raise Exception("Wrong input type, use a list of Simulations or WaterTopo objects.")
+    
+
+    duration = series[0].wd.shape[0]
+
+    height = series[0].wd.shape[1]
+    width = series[0].wd.shape[2]
+
+    seq_per_sim = duration-T-H
+    num_sims = len(series)
+
+    X = np.zeros((seq_per_sim*num_sims, T, channels, height, width))
+    Y = np.zeros((seq_per_sim*num_sims, H, channels-1, height, width))
+
+    for i,serie in enumerate(series):
+        j = i * seq_per_sim
+        for t in range(seq_per_sim):
+            if channels == 2:               
+                X[j+t:j+t+T, :,0,:,:] = np.tile(serie.topography, (T,1,1))
+                X[j+t:j+t+T, :,1,:,:] = serie.wd[t:t+T]
+                
+                Y[j+t+T : j+t+T+H, :,:,:,:] = serie.wd[t+T:t+T+H]
+
+            elif channels == 4:
+                X[j+t:j+t+T, :,0,:,:] = np.tile(serie.topography, (T,1,1))
+                X[j+t:j+t+T, :,1,:,:] = serie.wd[t:t+T]
+                X[j+t:j+t+T, :,2,:,:] = serie.wd[t:t+T]
+                X[j+t:j+t+T, :,3,:,:] = serie.wd[t:t+T]
+
+                Y[j+t+T: j+t+T+H, :,0,:,:] = serie.wd[t+T:t+T+H]
+                Y[j+t+T: j+t+T+H, :,1,:,:] = serie.vx[t+T:t+T+H]
+                Y[j+t+T: j+t+T+H, :,2,:,:] = serie.vy[t+T:t+T+H]
+
+    return X, Y
--- src/utils/watertopo.py
+++ src/utils/watertopo.py
@@ -41,23 +41,19 @@ class WaterTopo():
         for i in range(sim_amount):
             name = fnames[idx[i]]
 
-            coords = np.loadtxt(f"{save_folder}\\DEM\\" + name_dict["DEM"] + name[3:])[:, :2]
             topo = np.loadtxt(f"{save_folder}\\DEM\\" + name_dict["DEM"] + name[3:])[:, 2].reshape(number_grids,number_grids)
             wd = np.loadtxt(f"{save_folder}\\WD\\" + name_dict["WD"] + name[3:]).reshape(-1,number_grids,number_grids)
 
-            sims.append(WaterTopo(coords, topo, wd))
+            sims.append(WaterTopo(topo, wd))
 
         return sims
     
-    def __init__(self, coordinates, topography, wd):
+    def __init__(self, topography, wd):
         """
         A class that contains for a simulation:
         - topography:   2D array
         - wd:           3D array
-        - vx:           3D array
-        - vy:           3D array
         """
-        self.coordinates = coordinates
         self.topography = topography
         self.wd = wd
 
@@ -68,3 +64,21 @@ class WaterTopo():
         returns: wd, vx, vy at specified timestep
         """
         return self.wd[timestep]
+    
+    def plot_animation(self, cmap_topo="terrain", cmap_flood="Blues"):
+        fig, axs = plt.subplots(1,2,figsize=(8,4))
+        axs[0].imshow(self.topography, cmap=cmap_topo, origin="lower")
+
+        # Create list of images that are animated
+        imgs = []
+        for i in range(self.wd.shape[0]):
+            im = axs[1].imshow(self.wd[i], cmap=cmap_flood, origin='lower', animated=True)
+
+            if i==0:
+                axs[1].imshow(self.wd[i], cmap=cmap_flood, origin='lower')
+            imgs.append([im])
+
+        # Create the animation
+        ani = animation.ArtistAnimation(fig, imgs, interval=50, blit=True,
+                                        repeat_delay=1000)
+        return ani
