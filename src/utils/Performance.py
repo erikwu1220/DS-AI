13cc2530013c3f834ed375e829fc16cc9fb522d4
Han-Yu <h.wu-28@student.tudelft.nl>
2024-02-01 19:39:37 +0100

    add confusionmatrix

--- src/utils/Performance.py
+++ src/utils/Performance.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation, PillowWriter
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score, classification_report
+import seaborn as sns
+
 def Performance(prediction,Real,*args,**kwargs):
     '''
     This function is used to present the accuracy of the pridicted result.
@@ -16,7 +19,7 @@ def Performance(prediction,Real,*args,**kwargs):
         t = kwargs["timestep"]
 
         # Calculate accuracy
-        acc = 1-np.divide((prediction[t] - Real[t]),Real[t])
+        acc = 1-np.divide(abs((prediction[t] - Real[t])),Real[t])
         plt.figure(figsize=(6, 6))
         plt.imshow(acc, cmap='RdBu', origin='lower')
         plt.colorbar(plt.cm.ScalarMappable(norm=plt.Normalize(vmin = 0, vmax=1),
@@ -32,7 +35,7 @@ def Performance(prediction,Real,*args,**kwargs):
 
         for i in range(len(prediction)):
             # Calculate accuracy
-            acc = 1-np.divide((prediction[i] - Real[i]),Real[i])
+            acc = 1-np.divide(abs((prediction[i] - Real[i])),Real[i])
             ttl = plt.text(60, 5, f"timestep ={i}", horizontalalignment='right', verticalalignment='bottom')
             im = plt.imshow(acc,cmap='RdBu', animated=True, origin='lower')
 
@@ -51,3 +54,43 @@ def Performance(prediction,Real,*args,**kwargs):
 
         plt.show()
         print("done")
+
+def confusionmatrix(prediction,real,timestep):
+    '''
+    This function is used to built up the confusion matrix.
+    Pixel with water and without water will be classified into two different classes, and
+    from which calculate the accuracy, recall, and precisin.
+    Note that this function can not tell the accuracy of water depth within the specific pixel.
+    To better quantitatively study the model behaviours, please use "Performance" function.
+
+    prediction: prediction from the model
+    real: real value
+    timestep: specific timestep for confusion matrix
+    '''
+    pre = prediction[timestep].reshape(-1)
+    pre[pre <= 0] = 0
+    pre[pre > 0] = 1
+
+    rea = real[timestep].reshape(-1)
+    rea[rea <= 0] = 0
+    rea[rea>0] = 1
+
+    conf_matrix = confusion_matrix(rea, pre)
+    accuracy = accuracy_score(rea, pre)
+    recall = recall_score(rea, pre)
+    precision = precision_score(rea, pre)
+    f1 = f1_score(rea, pre)
+
+    plt.figure(figsize=(3, 3))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False)
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+    # Results
+    print("Confusion Matrix:\n", conf_matrix)
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"F1 Score: {f1:.4f}")
